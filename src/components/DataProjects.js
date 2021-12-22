d00d271d18d73c62c4a1df503516ed0eba9809a2
kevin calderon <kncalderong@gmail.com>
2021-12-21 22:07:48 -0500

    slide data-projects section css finished

--- src/components/DataProjects.js
+++ src/components/DataProjects.js
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import data from "../data/dv-projects";
 
 const DataProjects = () => {
+  const [index, setIndex] = useState(0);
+  const [projects, setProjects] = useState(data);
   return (
-    <article className="data-projects">Data visualization projects</article>
+    <article className="data-projects">
+      <h3 className="subtitle">Data visualization projects :</h3>
+      <div className="dv-container">
+        {projects.map((project, projectIndex) => {
+          const { id, name, url, info, img } = project;
+          return (
+            <div className="dv-item activeSlide" key={id}>
+              <a href={url} target="_blank" rel="noreferrer">
+                <img src={img} alt="rise of nations" className="dv-img" />
+              </a>
+              <h3>{name}</h3>
+              <p>{info}</p>
+            </div>
+          );
+        })}
+
+        <button className="prev">
+          <FiChevronLeft size={30} />
+        </button>
+        <button className="next">
+          <FiChevronRight size={30} />
+        </button>
+      </div>
+      <Link to="/projects" className="show-all">
+        Show All
+      </Link>
+    </article>
   );
 };
 
---
+++ src/data/dv-projects.js
@@ -0,0 +1,11 @@
+const dvProjects = [
+  {
+    id: 1,
+    name: "First title",
+    img: "https://phantom-marca.unidadeditorial.es/d6fc3e635a1e6571a192a4062f97f8b0/resize/1320/f/jpg/assets/multimedia/imagenes/2021/01/14/16106544161764.jpg",
+    info: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium tenetur obcaecati quo laborum cum? Veniam corrupti fuga iure quibusdam deleniti.",
+    url: "https://google.com",
+  },
+];
+
+export default dvProjects;
