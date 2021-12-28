43b624a20897bd9071a26b1de8000fab360c51f4
kevin calderon <kncalderong@gmail.com>
2021-12-27 22:07:43 -0500

    drumpad project added

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,16 +2,16 @@ const projects = [
   {
     id: 1,
     name: "Menu",
-    img: "./projects_images/menu.png",
-    info: "it's a menu UI, with a friendly way to show available products, rendered and filtered dynamically",
+    img: "./projects_images/menu1.png",
+    info: "It's a menu UI, with a friendly way to show available products, rendered and filtered dynamically",
     url: "https://kn-generalmenu-reactjsproject.netlify.app/",
   },
   {
     id: 2,
-    name: "initial project",
-    img: "http://adminassets.devops.arabiaweather.com/sites/default/files/field/image/mountains.jpg",
-    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit Perspiciatis corporis aliquam sapiente, nulla praesentium",
-    url: "https://google.com",
+    name: "Drumpad",
+    img: "./projects_images/drumpad.png",
+    info: "A single web application to play a wide range of sounds, interactive UI using both mouse as keyboard",
+    url: "https://kn-drumpad-react.netlify.app/",
   },
   {
     id: 3,
